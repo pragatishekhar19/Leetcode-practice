71cbd369fc20525c1f3da2fd9338cc9508438da5
Pragati Shekhar <ps03191017@gmail.com>
2024-02-13 18:55:11 +0530

    Create p12.c

---
+++ p12.c
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> pascal(numRows);
+        for (int i=0; i<numRows; i++){
+            pascal[i].resize(i+1);
+            for(int j=0; j<i+1; j++){
+                if((j==0)||(j==i)){
+                    pascal[i][j]=1;
+                }
+                else{
+                    pascal[i][j]=pascal[i-1][j]+pascal[i-1][j-1];
+                }
+            }
+        }
+        return pascal;
+        
+    }
+};
