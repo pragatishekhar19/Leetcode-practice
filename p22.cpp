4fd762163b39bdff36fe2d07b4b2531eee215e62
Pragati Shekhar <ps03191017@gmail.com>
2024-02-15 01:37:59 +0530

    Create p22.cpp

---
+++ p22.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        for(int i=0;i<s.size();i++){
+            int count=0;
+            for(int j=0;j<s.size();j++){
+                if((i!=j)&&(s[i]==s[j])){
+                    count++;
+                    break;
+                }
+            }
+            if (count==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
